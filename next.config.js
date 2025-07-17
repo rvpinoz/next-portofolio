ddd674a45d51eaa93cf81d474e0b863c9e16d4ea
Dimas Erlangga Putera <rvpinoz@gmail.com>
2025-07-17 09:24:05 +0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,10 +8,9 @@ const nextConfig = {
       },
     ],
   },
-  // REMOVE serverActions karena tidak kompatibel dengan static export
-  output: 'export', // penting untuk GitHub Pages
-  basePath: isProd ? '/rvpinoz.github.io' : '',
-  assetPrefix: isProd ? '/rvpinoz.github.io/' : '',
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
